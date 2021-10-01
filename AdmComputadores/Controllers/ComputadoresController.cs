7a0f32ea5ceea907fc62e912163b3673361b7eb3
burnout156 <brunoferreira378@gmail.com>
2021-10-01 13:51:20 -0300

    primeiro commit

---
+++ AdmComputadores/Controllers/ComputadoresController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AdmComputadores.Models;
+
+namespace AdmComputadores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComputadoresController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ComputadoresController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Computadores
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Computador>>> GetComputadores()
+        {
+            return await _context.Computadores.ToListAsync();
+        }
+
+        // GET: api/Computadores/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Computador>> GetComputador(int id)
+        {
+            var computador = await _context.Computadores.FindAsync(id);
+
+            if (computador == null)
+            {
+                return NotFound();
+            }
+
+            return computador;
+        }
+
+        // PUT: api/Computadores/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutComputador(int id, Computador computador)
+        {
+            if (id != computador.ComputadorId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(computador).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ComputadorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Computadores
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Computador>> PostComputador(Computador computador)
+        {
+            _context.Computadores.Add(computador);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetComputador", new { id = computador.ComputadorId }, computador);
+        }
+
+        // DELETE: api/Computadores/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteComputador(int id)
+        {
+            var computador = await _context.Computadores.FindAsync(id);
+            if (computador == null)
+            {
+                return NotFound();
+            }
+
+            _context.Computadores.Remove(computador);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ComputadorExists(int id)
+        {
+            return _context.Computadores.Any(e => e.ComputadorId == id);
+        }
+    }
+}
---
+++ AdmComputadores/Models/Computador.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AdmComputadores.Models
+{
+    public class Computador
+    {
+        [Key]
+        public int ComputadorId { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(15)")]
+        public string Marca { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(15)")]
+        public string Modelo { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(15)")]
+        public string PlacaMae { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(30)")]
+        public string MemoriaRAM { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(30)")]
+        public string HD { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(30)")]
+        public string MarcaHD { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(30)")]
+        public string VelocidadeProcessador { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(60)")]
+        public string Foto;
+    }
+}
