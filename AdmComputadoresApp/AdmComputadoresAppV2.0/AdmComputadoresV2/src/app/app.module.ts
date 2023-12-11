ffa71cc351b62e3a871b300b37f33a0f7dcf077a
Bruno Ferreira <brunoferreira378@gmail.com>
2023-12-11 14:14:25 -0300

    melhorias no layout com alinhamento

--- AdmComputadoresApp/AdmComputadoresAppV2.0/AdmComputadoresV2/src/app/app.module.ts
+++ AdmComputadoresApp/AdmComputadoresAppV2.0/AdmComputadoresV2/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { ApplicationRef, DoBootstrap, NgModule } from '@angular/core';
+import { ApplicationRef, DoBootstrap, NgModule, importProvidersFrom } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormControl, Validators, ReactiveFormsModule, FormGroup, FormsModule } from '@angular/forms';
@@ -15,7 +15,7 @@ import { CommonModule } from '@angular/common';
     FormsModule,
     CommonModule
   ],
-  providers: [AppService]
+  providers: [AppService, importProvidersFrom(HttpClientModule)]
 })
 export class AppModule implements DoBootstrap {
   constructor(){
